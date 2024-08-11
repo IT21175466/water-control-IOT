cb07742c3450f119aa1ec13af61972a927ef9b2e
Chamath <chamath@flexiata.lk>
2024-08-11 19:52:03 +0530

    Firebase Connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:watercontroliot/firebase_options.dart';
 import 'package:watercontroliot/splash_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
